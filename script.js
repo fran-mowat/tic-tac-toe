52459984ffdb67872996685b4238248d24271f71
fran0770 <franmowat33@gmail.com>
2023-11-19 13:22:48 +0000

    event listeners to add x when board square is clicked

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let squares = document.getElementsByClassName("board-square")
+//let square_contents = document.getElementsByClassName("board-text")
 
-/*
-for (let i=0; i<squares.length; i++){
-    squares[i].style.backgroundColor = "red"
+let set_square_contents = (e) => {
+    e.target.innerHTML = "x"
+    e.target.classList.add("x")
 }
-*/
+
+for (let i=0; i<squares.length; i++){
+    squares[i].addEventListener("click", set_square_contents)
+}
